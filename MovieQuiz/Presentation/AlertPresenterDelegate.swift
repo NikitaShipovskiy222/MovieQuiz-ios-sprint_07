1c15e7022ed9cda76c03915750411f0bc10b0f20
NikitaShipovskiy222 <nikita04.shipovskiy@gmail.com>
2024-04-24 17:23:21 +0300

    Initial commit

---
+++ MovieQuiz/Presentation/AlertPresenterDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AlertPresenterDelegate.swift
+//  MovieQuiz
+//
+//  Created by Сергей Баскаков on 24.01.2024.
+//
+
+import UIKit
+
+protocol AlertPresenterDelegate: AnyObject {
+    func show(alert: UIAlertController)
+}
